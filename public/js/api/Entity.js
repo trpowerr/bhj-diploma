dd11ac3af480e9e987ecd66a56656906824dd2c9
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-02-14 17:47:59 +0300

    fix remove

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -40,14 +40,18 @@ class Entity {
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove(data, callback ) {
+  static remove(id = '',data, callback ) {
   
 
     createRequest({
       url: this.URL,
       method: 'DELETE',
       responseType: 'json',
-      //data: formData,
+      data: {
+        ...data,
+        _method: 'DELETE',
+        id: id
+      },
       callback
     });
   }
